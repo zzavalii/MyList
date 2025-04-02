a4a32973a1f32435520bc06002d22d35482d5845
obutr <butrym@karazin.ua>
2025-04-02 20:56:10 +0300

    StateList

--- app/src/main/java/softserve/academy/mylist/MainActivity.kt
+++ app/src/main/java/softserve/academy/mylist/MainActivity.kt
@@ -5,22 +5,32 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import softserve.academy.mylist.ui.theme.MyListTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,32 +49,61 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-fun main() {
-    val item1 = ShoppingItem("Milk")
-}
-
 // Model View ViewModel
 
 data class ShoppingItem(
     val name: String,
-    var isBought: Boolean = false
+    val isBought: Boolean = false
 )
 
-@Composable
-fun ShoppingItemCard(item: ShoppingItem) {
+class ShoppingListViewModel : ViewModel() {
+    val shoppingList = mutableStateListOf(
+        ShoppingItem("Молоко"),
+        ShoppingItem("Хліб"),
+        ShoppingItem("Яйця"),
+        ShoppingItem("Масло"),
+        ShoppingItem("Олія"),
+        ShoppingItem("Авокадо"),
+        ShoppingItem("Ананас"),
+        ShoppingItem("Масло"),
+        ShoppingItem("Олія"),
+        ShoppingItem("Авокадо"),
+        ShoppingItem("Ананас"),
+        ShoppingItem("Ананас"),
+        ShoppingItem("Масло"),
+        ShoppingItem("Олія"),
+        ShoppingItem("Авокадо"),
+        ShoppingItem("Ананас"),
+    )
 
+    fun toggleBought(index: Int) {
+        shoppingList[index] = shoppingList[index].copy(
+            isBought = !shoppingList[index].isBought)
+    }
+}
+
+
+@Composable
+fun ShoppingItemCard(
+    item: ShoppingItem,
+    onToggleBought: () -> Unit = {}
+) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp)
             .background(
-                MaterialTheme.colorScheme.surfaceDim,
-                MaterialTheme.shapes.large)
-            .padding(16.dp)
-        ,
+                Color.LightGray,
+//                MaterialTheme.colorScheme.surfaceDim,
+                MaterialTheme.shapes.large
+            )
+            .clickable { onToggleBought() }
+            .padding(16.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Checkbox(checked = item.isBought, onCheckedChange = { })
+        Checkbox(checked = item.isBought, onCheckedChange = {
+            onToggleBought()
+        })
         Text(
             text = item.name,
             modifier = Modifier.weight(1f),
@@ -73,8 +112,36 @@ fun ShoppingItemCard(item: ShoppingItem) {
     }
 }
 
-@Preview(showBackground = true)
+
+@Composable
+fun ShoppingListScreen(viewModel: ShoppingListViewModel = viewModel()) {
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+            .padding(16.dp)
+    ) {
+        itemsIndexed(viewModel.shoppingList) { ix, item ->
+            ShoppingItemCard(item) {
+                viewModel.toggleBought(ix)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ShoppingListScreenPreview() {
+    ShoppingListScreen()
+}
+
+
+//@Preview(showBackground = true)
 @Composable
 fun ShoppingItemCardPreview() {
-    ShoppingItemCard(ShoppingItem("Молоко"))
+    var toggleState by remember { mutableStateOf(false) }
+    ShoppingItemCard(
+        ShoppingItem("Молоко", isBought = toggleState)
+    ) {
+        toggleState = !toggleState
+    }
 }
