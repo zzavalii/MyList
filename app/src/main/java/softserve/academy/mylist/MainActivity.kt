430366df8e44ffb145a6b0d53a5c6488430c331b
obutr <butrym@karazin.ua>
2025-04-02 19:46:54 +0300

    Style Card

--- app/src/main/java/softserve/academy/mylist/MainActivity.kt
+++ app/src/main/java/softserve/academy/mylist/MainActivity.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 import softserve.academy.mylist.ui.theme.MyListTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,10 +52,25 @@ data class ShoppingItem(
 
 @Composable
 fun ShoppingItemCard(item: ShoppingItem) {
-    Text(
-        text=item.name,
-        fontSize = 18.sp
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .background(
+                MaterialTheme.colorScheme.surfaceDim,
+                MaterialTheme.shapes.large)
+            .padding(16.dp)
+        ,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(checked = item.isBought, onCheckedChange = { })
+        Text(
+            text = item.name,
+            modifier = Modifier.weight(1f),
+            fontSize = 18.sp
         )
+    }
 }
 
 @Preview(showBackground = true)
